de7e72de008fa6ce9aa04036d0020ba5cfa35ac9
jbortiz0623 <jbortiz0623@gmail.com>
2018-11-09 09:02:04 -0800

    inserted new file name tags

---
+++ starter-code/script.js
@@ -0,0 +1,17 @@
+$(go).click(function)) {
+  function checkIfComplete() {
+    if ( iscomplete === false ) {
+      isComplete === true;
+     else {
+      place = 'second';
+    }
+
+  }
+}
+
+var carWidth = $('#car1').width();
+
+var raceTrackWidth = (window).width() - carWidth;
+
+var raceTime1 = Math.floor ( Math.random() * 5000) + 1);
+var raceTime2 = Math.floor ( Math.random() * 5000) + 1);
